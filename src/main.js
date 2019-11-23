1da35c9fbc3863cc2a2ac6a4959fce3d21ed3917
Гендольф <ecehuh67@gmail.com>
2019-11-23 13:54:26 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,81 +1,17 @@
-'use strict';
+import {createDetailInfoTemplate} from './components/detail.js';
+import {createUserRankTemplate} from './components/user-rank.js';
+import {createNavigationTemplate} from './components/navigation.js';
+import {createFiltersTemplate} from './components/filter.js';
+import {createFilmsPosterTemplate} from './components/poster.js';
+import {createFilmCardTempalate} from './components/card.js';
+import {createShowButtonTemplate} from './components/show-more-button.js';
+import {createTopRatedTemplate} from './components/top-rated.js';
+import {createMostCommentedTemplate} from './components/most-comment.js';
+import {createContainerPageTemplate} from './components/film-container.js';
+
 
 const FILM_COUNT = 5;
 const FILM_POPULAR = 2;
-
-const createUserRankTemplate = () => {
-  return `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>`;
-};
-
-const createNavigationTemplate = () => {
-  return `<nav class="main-navigation">
-    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-    <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
-  </nav>`;
-};
-
-const createFiltersTemplate = () => {
-  return `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`;
-};
-
-const createContainerPageTemplate = () => {
-  return `<section class="films"></section>`;
-};
-
-const createFilmsPosterTemplate = () => {
-  return `<section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container"></div>
-    </section>`;
-};
-
-const createFilmCardTempalate = () => {
-  return `<article class="film-card">
-    <h3 class="film-card__title">The Dance of Life</h3>
-    <p class="film-card__rating">8.3</p>
-    <p class="film-card__info">
-      <span class="film-card__year">1929</span>
-      <span class="film-card__duration">1h 55m</span>
-      <span class="film-card__genre">Musical</span>
-    </p>
-    <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-    <a class="film-card__comments">5 comments</a>
-    <form class="film-card__controls">
-      <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-      <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-      <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-    </form>
-  </article>`;
-};
-
-const createShowButtonTemplate = () => {
-  return `<button class="films-list__show-more">Show more</button>`;
-};
-
-const createTopRatedTemplate = () => {
-  return `<section class="films-list--extra">
-    <h2 class="films-list__title">Top rated</h2>
-    <div class="films-list__container"></div>
-  </section>`;
-};
-
-const createMostCommentedTemplate = () => {
-  return `<section class="films-list--extra">
-    <h2 class="films-list__title">Most commented</h2>
-    <div class="films-list__container"></div>
-  </section>`;
-};
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -119,3 +55,6 @@ ratedContainerElements.forEach(
       fillContainer(FILM_POPULAR, el, createFilmCardTempalate(), `beforeend`);
     }
 );
+
+// const bodyElement = document.querySelector(`body`);
+// render(bodyElement, createDetailInfoTemplate(), `beforeend`);
